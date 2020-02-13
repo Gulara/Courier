3a0d799350e2907c45f58db513519ae9fbb292dc
gulara <gularah1997@gmail.com>
2020-02-13 14:24:43 +0400

    js-refactoring

--- build/js/datepicker-for-index-8.js
+++ build/js/datepicker-for-index-8.js
@@ -1,54 +1,54 @@
 
-// //  DATEPICKER START
+// // //  DATEPICKER START
 
-$(document).ready(function () {
-    var date = new Date();
+// $(document).ready(function () {
+//     var date = new Date();
 
-    date.setDate(date.getDate());
-    $("#datepicker").datepicker({
-        startDate: date,
-        duration: "fast",
-        gotoCurrent: true,
-        firstDay: 1,
-        isRTL: false,
-        showMonthAfterYear: false,
-        dateFormat: "dd/mm/yy",
-        yearSuffix: "",
-        minDate: 1, // disable past date
-        minTime: 0, // disable past time
+//     date.setDate(date.getDate());
+//     $("#datepicker").datepicker({
+//         startDate: date,
+//         duration: "fast",
+//         gotoCurrent: true,
+//         firstDay: 1,
+//         isRTL: false,
+//         showMonthAfterYear: false,
+//         dateFormat: "dd/mm/yy",
+//         yearSuffix: "",
+//         minDate: 1, // disable past date
+//         minTime: 0, // disable past time
 
-        // VAXTIN İNPUT NAME=DAY-DƏ  VƏ VALUE-SUNDA GÖRSƏNMƏSİ
-        onSelect: function () {
-            $("label[for='calendar']").text($(this).val());
-            $("input[data-item-id=calendar-date]").attr('data-slash', ($(this).val()));
-            var calendarLabel = $(".calendar-label").text();
+//         // VAXTIN İNPUT NAME=DAY-DƏ  VƏ VALUE-SUNDA GÖRSƏNMƏSİ
+//         onSelect: function () {
+//             $("label[for='calendar']").text($(this).val());
+//             $("input[data-item-id=calendar-date]").attr('data-slash', ($(this).val()));
+//             var calendarLabel = $(".calendar-label").text();
            
-            var calendarVal = document.getElementById("calendar");
-            calendarVal.value = calendarLabel ;
-            $(this).change();
+//             var calendarVal = document.getElementById("calendar");
+//             calendarVal.value = calendarLabel ;
+//             $(this).change();
            
-        }
+//         }
 
         
-    }).on("change", function() {
-        $( "input[data-item-id=calendar-times]" ).prop( "checked", false );
+//     }).on("change", function() {
+//         $( "input[data-item-id=calendar-times]" ).prop( "checked", false );
         
-      });
-    // $("#datepicker").datepicker($.datepicker.regional["az"]); 
-    // $("#datepicker").datepicker($.datepicker.regional["ru"]); 
-    $("#datepicker").datepicker($.datepicker.regional["en-AU"]); 
+//       });
+//     // $("#datepicker").datepicker($.datepicker.regional["az"]); 
+//     // $("#datepicker").datepicker($.datepicker.regional["ru"]); 
+//     $("#datepicker").datepicker($.datepicker.regional["en-AU"]); 
   
 
-    $("#calendar").click(function () {
-        $("#datepicker").datepicker("show");
-    });
+//     $("#calendar").click(function () {
+//         $("#datepicker").datepicker("show");
+//     });
 
-    $(".ui-datepicker").css({
-        'box-shadow': '-2px 0px 17px 3px rgba(0,0,0,0.55)'
+//     $(".ui-datepicker").css({
+//         'box-shadow': '-2px 0px 17px 3px rgba(0,0,0,0.55)'
 
        
 
-    });
-});
+//     });
+// });
 
-// //  DATEPICKER END
+// // //  DATEPICKER END
--- build/js/delivery-date.js
+++ build/js/delivery-date.js
@@ -13,16 +13,6 @@ $(document).ready(function () {
           'display': 'block'
         });
 
-        // $('.wave-left').addClass('wave-down-left');
-        // $('.wave-right').addClass('wave-up-left');
-        // $('.wave-down-left').delay(1000).queue(function (next) {
-        //   $('.wave-left').removeClass('wave-down-left');
-        //   next();
-        // });
-        // $('.wave-up-left').delay(1000).queue(function (next) {
-        //   $('.wave-right').removeClass('wave-up-left');
-        //   next();
-        // });
 
 
       } else if ($(this).is(':checked') && $(this).val() == 'urgent') {
@@ -31,16 +21,6 @@ $(document).ready(function () {
           'display': 'none'
         });
 
-        // $('.wave-left').addClass('wave-up-right');
-        // $('.wave-right').addClass('wave-down-right');
-        // $('.wave-up-right').delay(1000).queue(function (next) {
-        //   $('.wave-left').removeClass('wave-up-right');
-        //   next();
-        // });
-        // $('.wave-down-right').delay(1000).queue(function (next) {
-        //   $('.wave-right').removeClass('wave-down-right');
-        //   next();
-        // });
       }
     });
 
@@ -48,31 +28,31 @@ $(document).ready(function () {
 
   // LOCAL STORAGE FOR INPUT RADIO 
 
-  $(function () {
-    $('input:radio[data-item-id="urgent"]').each(function () {
-      var state = JSON.parse(localStorage.getItem('radio_' + $(this).attr('id')));
+  // $(function () {
+  //   $('input:radio[data-item-id="urgent"]').each(function () {
+  //     var state = JSON.parse(localStorage.getItem('radio_' + $(this).attr('id')));
 
-      if (state) this.checked = state.checked;
-      if ($(this).is(':checked') && $(this).val() == 'non-urgent') {
-        $('#delivery').css({
-          'display': 'block'
-        });
+  //     if (state) this.checked = state.checked;
+  //     if ($(this).is(':checked') && $(this).val() == 'non-urgent') {
+  //       $('#delivery').css({
+  //         'display': 'block'
+  //       });
 
-      } else if ($(this).is(':checked') && $(this).val() == 'urgent') {
-        $('#delivery').css({
-          'display': 'none'
-        });
-      }
-    });
-  });
+  //     } else if ($(this).is(':checked') && $(this).val() == 'urgent') {
+  //       $('#delivery').css({
+  //         'display': 'none'
+  //       });
+  //     }
+  //   });
+  // });
 
-  $(window).bind('unload', function () {
-    $('input:radio[data-item-id="urgent"]').each(function () {
-      localStorage.setItem('radio_' + $(this).attr('id'), JSON.stringify({
-        checked: this.checked
-      }));
-    });
-  });
+  // $(window).bind('unload', function () {
+  //   $('input:radio[data-item-id="urgent"]').each(function () {
+  //     localStorage.setItem('radio_' + $(this).attr('id'), JSON.stringify({
+  //       checked: this.checked
+  //     }));
+  //   });
+  // });
 });
 
 // // DELIVERY DATE JS END
